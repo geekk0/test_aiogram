fd399f611ef4d5fc0de06e6a18208a187a83ec8c
gekk0 <gekk0dw@gmail.com>
2024-08-02 09:09:09 +0300

    weather api - weatherapi.com

--- app/weather_api.py
+++ app/weather_api.py
@@ -11,74 +11,34 @@ api_key = os.getenv('API_KEY')
 
 
 async def get_city_weather(city_name):
-    coordinates = await get_city_coordinates(city_name)
-    if coordinates:
-        weather = await get_location_weather(coordinates)
-        formatted_weather = format_weather(weather)
-        return formatted_weather
 
-
-async def get_city_coordinates(city_name):
-    url = 'http://api.openweathermap.org/geo/1.0/direct'
+    url = "http://api.weatherapi.com/v1/current.json"
 
     params = {
         'q': city_name,
-        'limit': 5,
-        'appid': api_key
-    }
-
-    headers = {
-        'Accept': 'application/json',
-        'User-Agent': 'test_aiogram'
-    }
-
-    max_retries = 5
-    backoff_factor = 1
-
-    async with httpx.AsyncClient() as client:
-        for attempt in range(max_retries):
-            try:
-                response = await client.get(url, params=params, headers=headers, timeout=10.0)
-                if response.status_code == 200:
-                    return {"latitude": response.json()[0]['lat'], 'longitude': response.json()[0]['lon']}
-            except httpx.RequestError as e:
-                logger.error(f"RequestError: {str(e)} \n attempt: {attempt}")
-                if attempt < max_retries - 1:
-                    await asyncio.sleep(backoff_factor * (2 ** attempt))
-                else:
-                    raise
-            except Exception as e:
-                logger.error(f"ReadError: {str(e)} \n attempt: {attempt}")
-                raise
-
-
-async def get_location_weather(coordinates: dict):
-    url = 'http://api.openweathermap.org/data/2.5/weather'
-    params = {
-        'lat': coordinates['latitude'],
-        'lon': coordinates['longitude'],
-        'appid': api_key,
-        'units': 'metric'
-    }
-
-    headers = {
-        'Accept': 'application/json',
-        'User-Agent': 'test_aiogram'
+        'key': api_key
     }
 
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(url, params=params, headers=headers, timeout=10.0)
+            response = await client.get(url, params=params, timeout=10.0)
             if response.status_code == 200:
-                return response.json()['main']
+                return format_weather(response.json()['current'])
         except Exception as e:
             logger.error(f"ReadError: {str(e)}")
 
 
 def format_weather(weather):
-    output = (f'Температура: {weather['temp']} C, Ощущается как: {weather['feels_like']} C\n '
-              f'Давление: {weather['pressure']}, Влажность: {weather['humidity']}')
+    output = (f'Температура: {weather['temp_c']}C \n '
+              f'Ощущеается как: {weather['feelslike_c']}С\n '
+              f'Скорость ветра: {weather['wind_kph']} Км\ч\n '
+              f'Давление: {weather['pressure_mb']} мм.рт.ст\n '
+              f'Влажность: {weather['humidity']}%')
     return output
+
+
+# weather = asyncio.run(get_city_weather('Chicago'))
+# print(format_weather(weather))
 
 
 
